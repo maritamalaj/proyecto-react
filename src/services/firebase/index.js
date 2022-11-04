e7b328c65f9c1e9dc8b57693f31c04ee0f48136b
maritamalaj <maritamalaj@hotmail.com>
2022-11-04 19:45:43 -0300

    error .env

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -4,23 +4,13 @@ import { initializeApp } from "firebase/app";
 import{ getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey:"AIzaSyCRKnkTC4BlWA2yO3Yyjtr3hcLBEqulU_c",
-  authDomain:"proyecto-react-coder22.firebaseapp.com",
-  projectId:"proyecto-react-coder22",
-  storageBucket:"proyecto-react-coder22.appspot.com",
-  messagingSenderId:"760314487879",
-  appId:"1:760314487879:web:d8b6c69ca72d7699fe3d62",
-
-};
-/*
-const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_apiKey_projectId,
   storageBucket: process.env.REACT_APP_apiKey_storageBucket,
   messagingSenderId: process.env.REACT_APP_apiKey_messagingSenderId,
   appId:process.env.REACT_APP_appId
-};*/
+};
 
 
 const app = initializeApp(firebaseConfig);
